bafe290b469747132ccfa9440aca78b57691149a
Matic Lulik <matic.lulik@gmail.com>
2022-03-01 22:57:17 +0100

    add initial models config

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   GET api/users
+// @route   POST api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'name is requires').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    //return res.status(200).json({ ok: 'ok' });
+    if (!errors.isEmpty()) {
+      //bad request if body is not OK
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+  }
+);
 
 module.exports = router;
