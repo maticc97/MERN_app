1a6190922aacca6fe58fb7e86b45a6bdbe4638c7
Matic Lulik <matic.lulik@gmail.com>
2022-03-01 20:53:30 +0100

    connection to mongo atlas

---
+++ config/db.js
@@ -0,0 +1,23 @@
+//connection to database
+const mongoose = require('mongoose');
+
+//require config package
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+
+        console.log("mongo DB connected")
+    } catch (err) {
+        console.error("MongoDB connect error: "+ err.message)
+        //exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
