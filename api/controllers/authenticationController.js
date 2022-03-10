d352d3effe3c2548516e033693a171b9878fa3f4
Matic Lulik <matic.lulik@gmail.com>
2022-03-10 22:01:19 +0100

    Programming JWT part

--- api/controllers/authenticationController.js
+++ api/controllers/authenticationController.js
@@ -1,12 +1,23 @@
 const res = require('express/lib/response');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 const User = require('../../models/User');
 const mongoose = require('mongoose');
 const req = require('express/lib/request');
 const { body } = require('express-validator');
 
+const passport = require('passport');
+
 const REQUIRED_FIELDS_ERR = 'Please provide all of the required fields -----> ';
 const REGEX_EMAIL = new RegExp('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}');
+
+async function encryptPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+  return password;
+}
 
 const addUser = (req, res) => {
   const { username, email, password } = req.body;
@@ -40,11 +51,24 @@ const addUser = (req, res) => {
               const user = new User();
               user.username = username;
               user.email = email;
-              const salt = await bcrypt.genSalt(10);
-              user.password = await bcrypt.hash(password, salt);
+              user.password = await encryptPassword(password);
               await user.save();
               //sucessfuly added
-              return res.status(200).send();
+              const payload = {
+                user: {
+                  id: user.id,
+                },
+              };
+              console.log('aa');
+              jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                  if (err) throw err;
+                  res.status(200).json({ token });
+                }
+              );
               //.json({ token: user.generateJwt() });
             }
           }
@@ -61,22 +85,22 @@ const addUser = (req, res) => {
 };
 
 const verifyUser = (req, res) => {
-  User
-    //check if email is taken
-    .findOne({ email: req.body.email })
-    .then(async (existingMailUser) => {
-      //if email is not taken, then check for taken username
-      if (!existingMailUser) {
-        res
-          .status(404)
-          .json({ errorMessage: 'Please check email or password' });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      'error ': REQUIRED_FIELDS_ERR + ' fullName, username, email, password',
+    });
+  } else {
+    passport.authenticate('local', (error, user, info) => {
+      if (error) return res.status(500).json(error);
+      if (user) {
+        res.status(200).json({ token: user.generateJwt() });
+      } else {
+        res.status(401).json(info);
       }
-      //if email is already taken
-      else {
-        
-      }
-    })
-    .catch((error) => renderApiError(req, res, error));
+    });
+  }
 };
 
 module.exports = {
